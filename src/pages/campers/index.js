2bdd833f49b710520326520ddffe0fa6c2493b3a
Radheya Mansel <rm@Radheyas-Mac-mini.local>
2023-07-06 14:59:33 +0100

    Add the code entry form to the campers index page

--- src/pages/campers/index.js
+++ src/pages/campers/index.js
@@ -1,7 +1,18 @@
+import styles from '../../styles/campersIndex.module.css'
+
 const Campers = () => {
   return (
     <div>
-      <h1>All Campers</h1>
+      <form className={styles.enterCodeForm}>
+        <div className={styles.inputWrapper}>
+          <label htmlFor='code-input'>Scan or Enter Code Manually</label>
+          <input id='code-input' className={styles.codeInput} type="number"/>
+        </div>
+        <div className={styles.inputWrapper}>
+          <button type="submit">Find</button>
+          <div className={styles.clearWrapper}><button>Clear</button></div>
+        </div>
+      </form>
     </div>
   );
 };
