40b11682762658ba6d264215f30fc17c8881da41
Radheya Mansel <rm@Radheyas-Mac-mini.local>
2023-07-10 12:15:58 +0100

    Improve the styling of the new transaction form

--- src/components/forms/NewTransForm.js
+++ src/components/forms/NewTransForm.js
@@ -4,21 +4,29 @@ import styles from './NewTransForm.module.css'
 const NewTransForm = props => {
   return (
     <form className={styles.newTransForm}>
-      <div>
-        <label htmlFor="deposit">Deposit</label>
-        <input type="number" id="deposit" />
+      <h2>Create Transactions</h2>
+      <p>Leave fields blank where not applicable, e.g. no withdrawal, leave blank...</p>
+      <p>Always enter positive transaction values.</p>
+      <div className={styles.inputs}>
+        <div>
+          <label htmlFor="deposit">Deposit</label>
+          <input type="number" id="deposit" />
+        </div>
+        <div>
+          <label type="number" htmlFor="book">Book</label>
+          <input type="number" id="book"/>
+        </div>
+        <div>
+          <label htmlFor="tuckshop">Tuckshop</label>
+          <input type="number" id="tuckshop"/>
+        </div>
+        <div>
+          <label htmlFor="withdraw">Withdraw</label>
+          <input type="number" id="withdraw"/>
+        </div>
       </div>
-      <div>
-        <label type="number" htmlFor="book">Book</label>
-        <input type="number" id="book"/>
-      </div>
-      <div>
-        <label htmlFor="tuckshop">Tuckshop</label>
-        <input type="number" id="tuckshop"/>
-      </div>
-      <div>
-        <label htmlFor="withdraw">Withdraw</label>
-        <input type="number" id="withdraw"/>
+      <div className={styles.submitBtn}>
+        <button type="submit">Add Transaction(s)</button>
       </div>
     </form>
   )
