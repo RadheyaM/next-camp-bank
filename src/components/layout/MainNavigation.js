aacd03395609bdfd534cf3f5a622bedd86dbeee6
Radheya Mansel <rm@Radheyas-Mac-mini.local>
2023-07-12 18:10:13 +0100

    add a material icon to the logo, style the main navigation

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,18 +1,26 @@
 import Link from "next/link";
 import styles from "./MainNavigation.module.css"
+import Button from '@mui/material/Button'
+import FestivalIcon from '@mui/icons-material/Festival';
 
 const MainNavigation = () => {
   return (
     <header className={styles.header}>
-      <Link href="/">
-        <h1>Carry On Camping!</h1>
-      </Link>
+      <div className={styles.logo}>
+        <Link href="/">
+          <span>
+            <FestivalIcon sx={{ fontSize: 60 }}/>
+            <h1>CARRY ON CAMPING</h1>
+            <hr />
+          </span>
+        </Link>
+      </div>
       <nav>
         <ul className={styles.navLinkBar}>
-          <li><Link href="/">Enter Code</Link></li>
-          <li><Link href="/campers" replace>All Campers</Link></li>
-          <li><Link href="/campers/transactions" replace>All Transactions</Link></li>
-          <li><Link href="/campers/add" replace>Add New Camper</Link></li>
+          <Button variant="contained" color="secondary"><Link href="/">Enter Code</Link></Button>
+          <Button variant="contained" color="secondary"><Link href="/campers" replace>All Campers</Link></Button>
+          <Button variant="contained" color="secondary"><Link href="/campers/transactions" replace>All Transactions</Link></Button>
+          <Button variant="contained" color="secondary"><Link href="/campers/add" replace>Add New Camper</Link></Button>
         </ul>
       </nav>
     </header>
