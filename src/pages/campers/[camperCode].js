f386d91b54dc02cadc66b3f1f05f81283b8fbc3b
Radheya Mansel <rm@Radheyas-Mac-mini.local>
2023-07-14 09:08:00 +0100

    Add server side rendering to the overview page

--- src/pages/campers/[camperCode].js
+++ src/pages/campers/[camperCode].js
@@ -63,34 +63,47 @@ const CamperOverview = (props) => {
 
 export default CamperOverview;
 
-export const getStaticProps = async (context) => {
+export const getServerSideProps = async (context) => {
   const { params } = context;
   const camperId = params.camperCode;
   const accountData = await getCamper(camperId);
   const transactionData = await getCamperTransactions(camperId);
-
   return {
     props: {
       camper: accountData,
       camperTrans: transactionData,
     },
-    revalidate: 10,
   };
 };
 
-export const getStaticPaths = async () => {
-  return {
-    paths: [
-      { params: { camperCode: "10001" } },
-      { params: { camperCode: "10002" } },
-      { params: { camperCode: "10003" } },
-      { params: { camperCode: "10004" } },
-      { params: { camperCode: "10005" } },
-      { params: { camperCode: "10006" } },
-      { params: { camperCode: "10007" } },
-      { params: { camperCode: "10008" } },
-      { params: { camperCode: "10009" } },
-    ],
-    fallback: false,
-  };
-};
+// export const getStaticProps = async (context) => {
+//   const { params } = context;
+//   const camperId = params.camperCode;
+//   const accountData = await getCamper(camperId);
+//   const transactionData = await getCamperTransactions(camperId);
+
+//   return {
+//     props: {
+//       camper: accountData,
+//       camperTrans: transactionData,
+//     },
+//     revalidate: 10,
+//   };
+// };
+
+// export const getStaticPaths = async () => {
+//   return {
+//     paths: [
+//       { params: { camperCode: "10001" } },
+//       { params: { camperCode: "10002" } },
+//       { params: { camperCode: "10003" } },
+//       { params: { camperCode: "10004" } },
+//       { params: { camperCode: "10005" } },
+//       { params: { camperCode: "10006" } },
+//       { params: { camperCode: "10007" } },
+//       { params: { camperCode: "10008" } },
+//       { params: { camperCode: "10009" } },
+//     ],
+//     fallback: false,
+//   };
+// };
