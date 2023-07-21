d4f1c11d6e15098b27f4025fc8d18863f5ba5537
Radheya Mansel <radheyamansel@hotmail.com>
2023-07-21 13:45:55 +0100

    remove api session protection on add camper

--- src/pages/api/campers/addCamper.js
+++ src/pages/api/campers/addCamper.js
@@ -1,25 +1,15 @@
 import clientPromise from "../../../../lib/db";
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "../auth/[...nextauth]"
 
 async function handler(req, res) {
-  const session = await getServerSession(res, res, authOptions)
-  if (session) {
-    try {
-      const client = await clientPromise;
-      const db = client.db("Campers");
-      const col = db.collection("Campers");
-      const insert = await col.insertOne(req.body);
-      res.status(201).json({ message: "success!" });
-    } catch (error) {
-      console.log("error: ", error);
-    }
-  } else {
-    res.send({
-      error: "You must log in to access this page."
-    })
+  try {
+    const client = await clientPromise;
+    const db = client.db("Campers");
+    const col = db.collection("Campers");
+    const insert = await col.insertOne(req.body);
+    res.status(201).json({ message: "success!" });
+  } catch (error) {
+    console.log("error: ", error);
   }
-  
 };
 
 export default handler;
