987da8c025d1400c6d5f7cbfc2e871b1f5a06547
Radheya Mansel <radheyamansel@hotmail.com>
2023-08-01 11:42:15 +0100

    get the site settings button to display only if session user is Superuser

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -3,8 +3,8 @@ import styles from "./MainNavigation.module.css";
 import { useSession, signOut } from "next-auth/react";
 import { Button } from "@mui/material";
 
-const MainNavigation = () => {
-  const {data: session} = useSession();
+const MainNavigation = (props) => {
+  const { data: session, status } = useSession();
   const logoutHandler = () => {
     signOut();
   };
@@ -14,34 +14,36 @@ const MainNavigation = () => {
       <div className={styles.logo}>
         <span>
           <div>
-          <Link href="/">
-            <div className={styles.logoImage}>
+            <Link href="/">
+              <div className={styles.logoImage}>
                 {/* <FestivalIcon sx={{ fontSize: 60 }} /> */}
                 <h3 className={styles.logoSubtext}>...intense banking</h3>
-            </div>
-          </Link>
+              </div>
+            </Link>
           </div>
         </span>
       </div>
-        <nav>
-            <ul className={styles.navLinkBar}>
-                  <Link href="/">Find Camper</Link>
-                  <Link href="/campers" replace>
-                    All Campers
-                  </Link>
-                  <Link href="/campers/transactions" replace>
-                    Recent Transactions
-                  </Link>
-            </ul>
-          {session && (
-            <div className={styles.logoutBtnDiv}>
-              <Button size="small" variant="contained" onClick={logoutHandler}>
-                  {session.user.email}  |  Logout
-              </Button>
-              <Button onClick={() => {}} size="small" variant="contained">Site Settings</Button>
-            </div>
-          )}
-        </nav>
+      <nav>
+        <ul className={styles.navLinkBar}>
+          <Link href="/">Find Camper</Link>
+          <Link href="/campers" replace>
+            All Campers
+          </Link>
+          <Link href="/campers/transactions" replace>
+            Recent Transactions
+          </Link>
+        </ul>
+        {session && (
+          <div className={styles.logoutBtnDiv}>
+            <Button size="small" variant="contained" onClick={logoutHandler}>
+              {session.user.email} | Logout
+            </Button>
+            {session.user.email === "Superuser" && <Button onClick={() => {}} size="small" variant="contained">
+              Site Settings
+            </Button>}
+          </div>
+        )}
+      </nav>
     </header>
   );
 };
